d05bea236605ae654db0121a46ca43ea97a06120
denev <denev723@gmail.com>
2026-05-22 15:13:56 +0900

    feat: shadcn 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <div className="text-3xl font-bold flex flex-col items-center">
+      <h1>Welcome to Comapre Phones</h1>
+      <Button className="w-full">Cleck me</Button>
+    </div>
+  );
 }
